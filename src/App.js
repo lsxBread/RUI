4a7dd9dcd7f6526fb905358d3bc03778ac1de322
Shixun Liu <shixun.liu1023@gmail.com>
2018-12-04 21:44:15 +1100

    click button and show loading spin

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App extends React.Component {
   render () {
     return (
       <React.Fragment>
-        <Button>Hello World</Button>
-        <Button icon='settings'>Hello</Button>
-        <Button icon='settings' iconPosition='right'>Hello</Button>
+        <Button loading={true} enableLoading={true}>Hello World</Button>
+        <Button icon='settings' enableLoading={true}>Hello</Button>
+        <Button icon='settings' iconPosition='right' enableLoading={true}>Hello</Button>
       </React.Fragment>
     )
   }
--- src/Button.js
+++ src/Button.js
@@ -3,29 +3,43 @@ import PropTypes from 'prop-types'
 import Icon from './Icon'
 import './scss/Button.scss'
 
-const Button = (props) => {
-  const { icon, iconPosition } = props
-  return (
-    <button className={`g-button icon-${iconPosition}`}>
-      <Icon className='loading' name='loading' />
-      {
-        props.icon &&
-        <Icon className="icon" name={icon} />
-      }
-      <div className='content'>
-        {props.children}
-      </div>
-    </button>
-  )
+class Button extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      loading: false
+    }
+  }
+  render () {
+    const { icon, iconPosition, enableLoading, children } = this.props
+    const { loading } = this.state
+    return (
+      <button className={`g-button icon-${iconPosition}`} onClick={() => this.setState({loading: !loading})}>
+        {
+          enableLoading && loading &&
+          <Icon className='loading icon' name='loading' />
+        }
+        {
+          ((icon && enableLoading && !loading) || (icon && !enableLoading)) &&
+          <Icon className="icon" name={icon} />
+        }
+        <div className='content'>
+          {children}
+        </div>
+      </button>
+    )
+  }
 }
 
 Button.propTypes = {
   icon: PropTypes.string,
-  iconPosition: PropTypes.oneOf(['left', 'right'])
+  iconPosition: PropTypes.oneOf(['left', 'right']),
+  enableLoading: PropTypes.bool
 }
 
 Button.defaultProps = {
-  iconPosition: 'left'
+  iconPosition: 'left',
+  loading: false
 }
 
 export default Button
