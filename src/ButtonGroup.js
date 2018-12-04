0a9573a49485ea0bed2f9163b1a7cb0b876d6636
Shixun Liu <shixun.liu1023@gmail.com>
2018-12-04 22:24:17 +1100

    check ButtonGroup children type ;

--- src/ButtonGroup.js
+++ src/ButtonGroup.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import './scss/ButtonGroup.scss'
 
-const ButtonGroup  = (props) => {
-  return (
-    <div className='g-button-group'>
-      {props.children}
-    </div>
-  )
+class ButtonGroup extends React.Component {
+  render () {
+    return (
+      <div className='g-button-group'>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+ButtonGroup.propTypes = {
+  children: (props, propName, componentName) => {
+    const prop = props[propName]
+    let error = null
+    React.Children.forEach(prop, (child) => {
+      if (child.type.name !== 'Button') {
+        error = new Error('`' + componentName + '` children should be of type `Button`.');
+      }
+    })
+    return error
+  }
 }
 
 export default ButtonGroup
